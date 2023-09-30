631af4337e327c9f0ec10c574ccf0a9b25ed0fd6
MatAndreoli <andreoli.2003@gmail.com>
2023-09-30 18:13:09 -0300

    trigger vercel

--- run_all.sh
+++ run_all.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 fuser -k 3000/tcp
+
+pip install virtualenv
+VIRTUAL_ENV=venv
+
+virtualenv -p python3.11 ${VIRTUAL_ENV}
+PATH="$VIRTUAL_ENV/bin:$PATH"
+
 . venv/bin/activate
+${VIRTUAL_ENV}/bin/pip install -r requirements.txt
 
 cd WebScraper/
-python3 main.py &
+${VIRTUAL_ENV}/bin/python main.py &
+
 cd ..
 npm start
